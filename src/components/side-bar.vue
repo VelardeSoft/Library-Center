9d1a741b6deee17121584f063b1576fbd592ca11
Velarde Gonzales Nestor H <u20211c221@upc.edu.pe>
2024-08-28 19:21:53 -0500

    feat: finaly.

--- src/components/side-bar.vue
+++ src/components/side-bar.vue
@@ -9,24 +9,34 @@ export default defineComponent({
   <aside class="side-bar">
     <nav>
       <ul>
-        <li>Books</li>
-        <li>Authors</li>
-        <li>Genres</li>
+        <li class="btn">Books</li>
+        <li class="btn">Authors</li>
+        <li class="btn">Genres</li>
       </ul>
     </nav>
   </aside>
 </template>
 
 <style scoped>
+.btn {
+  cursor: pointer;
+  background-color: #2c3e50;
+  color: white;
+  padding: 5px;
+  margin-right: 150px;
+  border-radius: 10px;
+}
+.btn:hover {
+  background-color: aqua;
+}
 .side-bar {
-  width: 300px;
+  width: 250px;
   background-color: #513c86;
   padding: 10px;
   position: fixed;
   top: 60px;
   left: 0;
   height: 83%;
-  border-radius: 20px;
 }
 nav ul {
   list-style-type: none;
